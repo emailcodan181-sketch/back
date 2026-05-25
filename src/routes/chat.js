1da4bf24d61bdd9ab56683c1bf18897dd155ac6f
codan <emailcodan181@gmail.com>
2026-05-25 00:28:12 -0300

    add conversations route

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -5,5 +5,6 @@ const verifyJWT = require('../middleware/verifyJWT');
 const { chatLimiter } = require('../middleware/rateLimiter');
 
 router.post('/', verifyJWT, chatLimiter, chatController.sendMessage);
+router.get('/conversations', verifyJWT, chatController.getConversations);
 
 module.exports = router;
