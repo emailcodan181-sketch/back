e3c350a2cfdd7d5eaaf3ce98c4ee339d04efe4c3
codan <emailcodan181@gmail.com>
2026-05-24 23:37:54 -0300

    fix: corrige listCodes para getCodes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -7,10 +7,12 @@ const { adminLimiter } = require('../middleware/rateLimiter');
 
 router.use(verifyJWT, verifyAdmin, adminLimiter);
 
-router.get('/codes', adminController.listCodes);
+router.get('/codes', adminController.getCodes);
 router.post('/codes/generate', adminController.generateCode);
 router.delete('/codes/:id', adminController.revokeCode);
 router.get('/logs', adminController.getLogs);
 router.get('/metrics', adminController.getMetrics);
+router.get('/users', adminController.getUsers);
+router.patch('/users/:id/promote', adminController.promoteUser);
 
 module.exports = router;
